28f4fb9019758c3754ae2e30f33c6262798a5dfa
Aliasgar Khimani <68768969+NovusEdge@users.noreply.github.com>
2025-04-20 14:23:15 +0300

    Adding project docs and dataset numbers

---
+++ lib/consts.scala
@@ -0,0 +1,15 @@
+package lib;
+
+object consts {
+    val API_URL = "https://data.fingrid.fi/api/datasets/"
+    val TIMEOUT = 5000 // in milliseconds
+    val API_KEY = "3653879acc754a97b69d05bdb857f9d9"
+
+    // Dataset IDs
+    val SURPLUS_EPROD_ID                = 362 // https://data.fingrid.fi/en/datasets/362
+    val ECONSUMPTION_FORECAST_ID        = 165 // https://data.fingrid.fi/en/datasets/165
+    val WIND_GENERATION_FORECAST_ID     = 246 // https://data.fingrid.fi/en/datasets/246
+    val SOLAR_GENERATION_FORECAST_ID    = 247 // https://data.fingrid.fi/en/datasets/247
+    val SOLAR_PRODUCTION_CAPACITY_ID    = 267 // https://data.fingrid.fi/en/datasets/267
+    val WIND_PRODUCTION_CAPACITY_ID     = 268 // https://data.fingrid.fi/en/datasets/268
+}
